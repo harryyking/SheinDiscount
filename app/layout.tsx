e68c7240376f10ee704477b6d1b575b39072061b
harryyking <arthurharry06@gmail.com>
2025-03-19 13:33:06 +0000

    new add

--- app/layout.tsx
+++ app/layout.tsx
@@ -16,7 +16,7 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en" data-theme={"forest"}>
+    <html lang="en" data-theme={"emerald"}>
       <body
         className={brandFont.className}
       >
